b3b9102cd45c5b925cea04a8e5396fe8507ca418
Toshihiko Fukushima <tsfk9981@gmail.com>
2025-05-31 20:41:31 +0200

    update package.xml and setup.py

--- src/continuous_navigator/setup.py
+++ src/continuous_navigator/setup.py
@@ -4,7 +4,7 @@ package_name = 'continuous_navigator'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -14,9 +14,9 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='fukushima',
-    maintainer_email='fukushima@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='tsfk9981@gmail.com',
+    description='TurtleBot3 continuously moves in a world',
+    license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
